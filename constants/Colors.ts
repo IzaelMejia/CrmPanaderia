f0a17f5d9917e70eef65cf9ef89f8ab39d008754
Izael Mejia <izaelmejiaa@gmail.com>
2025-06-12 14:04:31 -0600

    :sparkles: Estilos globales y constante de colores

---
+++ constants/Colors.ts
@@ -0,0 +1,4 @@
+export const Colors = {
+  primary: "red", 
+  opacity: "rgba(0, 0, 0, 0.5)",
+};
---
+++ globals/global-styles.ts
@@ -0,0 +1,10 @@
+import { Colors } from "@/constants/Colors";
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  opacityScreen: {
+    flex: 1,
+    backgroundColor: Colors.opacity,
+  },
+  
+});
